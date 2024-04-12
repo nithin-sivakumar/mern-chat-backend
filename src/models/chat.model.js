a9414cc9635340d0d053d426d719c543af2d1129
Nithin <nithinsgayathri@gmail.com>
2024-04-12 16:10:01 +0530

    Schema definitions

---
+++ src/models/chat.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Chat = mongoose.model("Chat", chatSchema);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,17 +4,17 @@ import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
-    username: {
+    name: {
       type: String,
-      required: [true, "Field not found. Username is required!"],
-      unique: true,
-      lowercase: true,
+      required: [true, "Field not found. Name is required!"],
       trim: true,
       index: true,
     },
-    fullName: {
+    email: {
       type: String,
-      required: [true, "Field not found. Name is required!"],
+      required: [true, "Field not found. Email is required!"],
+      unique: true,
+      lowercase: true,
       trim: true,
       index: true,
     },
@@ -44,8 +44,8 @@ userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-      fullName: this.fullName,
-      username: this.username,
+      name: this.name,
+      email: this.email,
     },
     process.env.ACCESS_TOKEN_SECRET,
     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
